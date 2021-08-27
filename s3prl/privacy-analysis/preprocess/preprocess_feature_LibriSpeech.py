582728a2a5b8a7e85e340236acd45bde546c3f75
RayTzeng <wiz94156.eed06@nctu.edu.tw>
2021-08-27 11:23:40 +0800

    add preprocess file

---
+++ s3prl/privacy-analysis/preprocess/preprocess_feature_LibriSpeech.py
@@ -0,0 +1,81 @@
+import os
+import glob
+import argparse
+import time
+from tqdm import tqdm
+
+import torch
+import torch.nn as nn
+import numpy as np
+import torchaudio
+from torchaudio.sox_effects import apply_effects_file
+from torch.utils.data.dataset import Dataset
+from torch.utils.data import DataLoader
+
+import IPython
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+
+def main(args):
+    model = torch.hub.load('s3prl/s3prl', args.model).to(device)
+    model.eval()
+
+    split_path = os.path.join(args.base_path, args.split)
+    
+    speaker_count = 0
+    audio_count = 0
+    for speaker in tqdm(glob.glob(os.path.join(split_path, '*[!.txt]')), ascii=True, desc="Speaker"):
+        speaker_count += 1
+        for chapter in glob.glob(os.path.join(split_path, speaker, '*')):
+
+            # check if output folder exist
+            output_folder = os.path.join(args.output_path, args.split, speaker.split("/")[-1], chapter.split("/")[-1])
+            # print(args.output_path)
+            # print(output_folder)
+            if not os.path.exists(output_folder):
+                os.makedirs(output_folder)
+                tqdm.write(f"Directory {output_folder} Created ")
+
+            # 
+            for audio_path in glob.glob(os.path.join(split_path, speaker, chapter,'*.flac')):
+                audio_count += 1
+
+                audio_name = audio_path.split("/")[-1]
+
+                wav, _ = apply_effects_file(
+                    audio_path,
+                    [
+                        ["channels", "1"],
+                        ["rate", "16000"],
+                        ["norm"],
+                    ],
+                )
+                wav = wav.squeeze(0).to(device)
+                with torch.no_grad():
+                    feature = model([wav])['last_hidden_state']
+
+                output_path = os.path.join(args.output_path, args.split, speaker.split("/")[-1], chapter.split("/")[-1], f"{args.model}-{audio_name}.pt")
+                tqdm.write(output_path)
+                torch.save(feature.cpu(), output_path)
+                
+                
+
+    print("There are {} speakers".format(speaker_count))
+    print("There are {} audios".format(audio_count))
+
+
+
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--base_path", help="directory of LibriSpeech dataset")
+    parser.add_argument("--split", help="which split of LibriSpeech")
+    parser.add_argument("--output_path", help="directory to save feautures")
+    parser.add_argument("--model", help="which self-supervised model to extract features")
+    args = parser.parse_args()
+
+    
+
+    main(args)
