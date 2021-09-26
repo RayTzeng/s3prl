3d992d68e2f91e848b5222d7bfcc63c647e04834
sky1456723 <b05901009@ntu.edu.tw>
2021-09-26 14:43:59 +0800

    Prepare for merge

--- s3prl/privacy-analysis/attack/run_speaker_level_attack.sh
+++ s3prl/privacy-analysis/attack/run_speaker_level_attack.sh
@@ -3,8 +3,9 @@
 # python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "modified_cpc"
 # python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "tera"
 
-python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "tera_100hr"
+#python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "tera_100hr"
 
-python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "wav2vec2_large_960"
+#python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "wav2vec2_large_960"
 
-python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "wav2vec2_large_ll60k"
+#python speaker-level-MIA.py --base_path "/home/raytz/Disk/LibriSpeech-Feature/" --output_path "/home/raytz/Disk/" --model "wav2vec2_large_ll60k"
+python speaker-level-MIA.py --base_path "/mnt/storage2/speech_data/LibriSpeech-Feature/" --output_path "/mnt/storage1/privacy/s3prl/s3prl/privacy-analysis/attack/results_def_100/" --model "pretrain_libri_reverse"
